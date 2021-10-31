dcb5ae98e015a9095e524466082c10cb82e108e4
Fabio <alessf13@gmail.com>
2021-10-31 14:34:59 +0100

    feat: added variable for number of pics to show

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
-const url = `https://api.pexels.com/v1/curated?per_page=10`;
+let numberOfPics = 12;
+const url = `https://api.pexels.com/v1/curated?per_page=${numberOfPics}`;
 const API_KEY = "563492ad6f91700001000001efd789b14aec4c9db33942e8a8dbf36e";
 let searchValue;
 // Evant Listeners
@@ -34,11 +35,12 @@ function generatePictures(data) {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
     galleryImg.innerHTML = `
+						<img src=${photo.src.large}></img>
 						<div class="gallery-info">			
 								<p>${photo.photographer}</p>
 								<a href=${photo.src.original}>Download</a>
 						</div>
-						<img src=${photo.src.large}></img>`;
+						`;
     gallery.appendChild(galleryImg);
   });
 }
@@ -50,7 +52,7 @@ async function curatedPhotos() {
 
 async function searchPhotos(query) {
   clear();
-  const urlSearch = `https://api.pexels.com/v1/search?query=${query}+query&per_page=10`;
+  const urlSearch = `https://api.pexels.com/v1/search?query=${query}+query&per_page=${numberOfPics}`;
 
   const data = await fetchAPI(urlSearch);
   generatePictures(data);
