baa761ebdfcb85714306dd04e80e7847c3a7e589
Fabio <alessf13@gmail.com>
2021-10-31 14:05:56 +0100

    refactor: async functions

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// Variables
+let searchValue;
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
-let searchValue;
-const url = `https://api.pexels.com/v1/curated?per_page=10`;
+const numberOfPics = 10;
+const url = `https://api.pexels.com/v1/curated?per_page=${numberOfPics}`;
 const API_KEY = "563492ad6f91700001000001efd789b14aec4c9db33942e8a8dbf36e";
 
 // Evant Listeners
@@ -12,12 +14,14 @@ form.addEventListener("submit", (e) => {
   searchPhotos(searchValue);
 });
 
+// Functions
 function updateInput(e) {
   searchValue = e.target.value;
 }
 
-async function curatedPhotos() {
-  const dataFetch = await fetch(url, {
+async function fetchAPI(url) {
+  const dataFetch = await (url,
+  {
     method: "GET",
     headers: {
       Accept: "application/json",
@@ -25,7 +29,10 @@ async function curatedPhotos() {
     },
   });
   const data = await dataFetch.json();
-  console.log(data.photos);
+  return data;
+}
+
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
@@ -35,24 +42,16 @@ async function curatedPhotos() {
   });
 }
 
+async function curatedPhotos() {
+  const data = await fetchAPI(url);
+  generatePictures(data);
+}
+
 async function searchPhotos(query) {
-  const urlSearch = `https://api.pexels.com/v1/search?query=${query}+query&per_page=10`;
-  const dataFetch = await fetch(urlSearch, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: API_KEY,
-    },
-  });
-  const data = await dataFetch.json();
-  console.log(data.photos);
-  data.photos.forEach((photo) => {
-    const galleryImg = document.createElement("div");
-    galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = ` <img src=${photo.src.large}></img>
-		<p>${photo.photographer}</p>`;
-    gallery.appendChild(galleryImg);
-  });
+  const urlSearch = `https://api.pexels.com/v1/search?query=${query}+query&per_page=${numberOfPics}`;
+
+  const data = await fetchAPI(urlSearch);
+  generatePictures(data);
 }
 
 curatedPhotos();
