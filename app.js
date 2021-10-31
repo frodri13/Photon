2d1730724057f4ec248556808703ecfc01c7b831
Fabio <alessf13@gmail.com>
2021-10-31 13:55:04 +0100

    feat: search fnctionality implemented

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
 const url = `https://api.pexels.com/v1/curated?per_page=10`;
 const API_KEY = "563492ad6f91700001000001efd789b14aec4c9db33942e8a8dbf36e";
 
+// Evant Listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
 async function curatedPhotos() {
   const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: API_KEY,
+    },
+  });
+  const data = await dataFetch.json();
+  console.log(data.photos);
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = ` <img src=${photo.src.large}></img>
+		<p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(query) {
+  const urlSearch = `https://api.pexels.com/v1/search?query=${query}+query&per_page=10`;
+  const dataFetch = await fetch(urlSearch, {
     method: "GET",
     headers: {
       Accept: "application/json",
